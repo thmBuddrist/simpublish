f9e79b960249a07440b73141c2a81d3cf3a59b69
thmbuddrist <thmBuddrist@gmail.com>
2022-06-03 13:37:51 +0800

    Auto commit.

--- autopush.bat
+++ autopush.bat
@@ -1,4 +1,4 @@
-@echo off #打开回显或关闭请求回显功能，off 可以改成 on。
+@echo  on#打开回显或关闭请求回显功能，off 可以改成 on。
 @title bat execute git auto commit #运行时命令行窗口的 title
 D:
 cd D:\Life\Learn\Github\simpublish #这里是要提交的项目目录，需要更改成自己的文件目录
